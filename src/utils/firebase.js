96f6c247fd7f323e793ed749cfd80daf4b7a891c
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-12-20 23:59:25 -0600

    Add firebase auth

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAW0nl_Prf2FEBUzS2KjDt0U0ZsjWhKuOI",
@@ -11,3 +13,17 @@ const firebaseConfig = {
   
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+}
